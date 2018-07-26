bb830d88485dc0c162fb58afaea06b6f2783680c
zhuzengding <zhuzengding@cmbchina.com>
2018-07-26 16:45:42 +0800

    baidu语音识别framework

---
+++ PBBaiduVoice/PBBaiduVoice.h
@@ -0,0 +1,24 @@
+//
+//  PBBaiduVoice.h
+//  PBBaiduVoice
+//
+//  Created by admin on 2018/7/26.
+//  Copyright © 2018年 zzd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PBBaiduVoice.
+FOUNDATION_EXPORT double PBBaiduVoiceVersionNumber;
+
+//! Project version string for PBBaiduVoice.
+FOUNDATION_EXPORT const unsigned char PBBaiduVoiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBBaiduVoice/PublicHeader.h>
+
+
+#import <PBBaiduVoice/BDSWakeupDefines.h>
+#import <PBBaiduVoice/BDSASRDefines.h>
+#import <PBBaiduVoice/BDSASRParameters.h>
+#import <PBBaiduVoice/BDSEventManager.h>
+#import <PBBaiduVoice/BDSWakeupParameters.h>
